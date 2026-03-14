270e37f63b08ea16e55ba4a587704c69a9cdd16b
huncholane <huncho@hygo.ai>
2026-03-13 18:03:08 -0700

    fix: tmux attach alias

--- config.sh
+++ config.sh
@@ -16,7 +16,7 @@ alias nvm=fnm
 alias ll='lsd -l'
 alias pip='uv pip'
 alias yay=paru
-alias t='tmux || tmux attach'
+alias t='tmux attach || tmux'
 
 # History substring search
 bindkey '^[[A' history-substring-search-up   # or '\eOA'
