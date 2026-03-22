c5f61cc956e37c145d84d1f0a9a4e40458e85aeb
huncholane <huncho@hygo.ai>
2026-03-21 17:25:21 -0700

    test: docker run script adds install script to binary path

--- install_test.sh
+++ install_test.sh
@@ -2,7 +2,11 @@
 
 case "$1" in
 "ubuntu") docker run --rm -v "$(pwd):/installer" -w /installer -it ubuntu bash -c "
-  apt update -y && apt install sudo -y && useradd test -m && echo test:test | chpasswd && usermod -aG sudo test &&
-  su - test" ;;
-"arch") docker run --rm -v "$(pwd):/installer" -w /installer -it arch 'pacman -Syu --noconfirm && pacman -S && pacman -S --noconfirm && useradd test && bash' ;;
+  apt update -y && apt install sudo -y && useradd test -m && echo test:test | chpasswd && usermod -aG sudo test && 
+  ln -s /installer/install.sh /bin/doit && su - test" ;;
+"arch") docker run --rm -v "$(pwd):/installer" -w /installer -it archlinux bash -c "
+  pacman --noconfirm -Syu && pacman -S sudo --noconfirm && useradd test -m && usermod -aG wheel test && 
+  echo test:test | chpasswd && sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers && 
+  ln -s /installer/install.sh /bin/doit && su - test" ;;
+*) echo "usage: install_test.sh [ubuntu|arch]"
 esac
