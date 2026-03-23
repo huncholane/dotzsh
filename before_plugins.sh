00c47635b00c01e51fe5562a1599386240faa87c
huncholane <huncho@hygo.ai>
2026-03-22 19:27:44 -0700

    refactor: use tput smcup/rmcup instead of animations

--- before_plugins.sh
+++ before_plugins.sh
@@ -1,13 +1,3 @@
-zfunc_dir="$ZDOTDIR/.zfunc"
-if [ ! -d "$zfunc_dir" ]; then mkdir -p "$zfunc_dir" &>/dev/null; fi
-
-add_completions() {
-  local file="$zfunc_dir/$1"
-  if [ ! -f "$file" ]; then
-    eval "$2" >"$file"
-  fi
-}
-
 # Update PATH
 export PATH="$PATH:$HOME/.local/bin:$HOME/go/bin:$HOME/.avm/bin:/usr/local/go/bin"
 export PATH="$PATH:$HOME/.local/share/solana/install/active_release/bin"
@@ -19,7 +9,15 @@ export PATH="$HOME/.local/share/fnm:$PATH"
 [ -f ~/.venv/bin/activate ] && . ~/.venv/bin/activate
 eval $(fnm env)
 
-# Create completions now that path is updated
+# Create completions into ~/.config/.zfuncs directory
+zfunc_dir="$ZDOTDIR/.zfunc"
+if [ ! -d "$zfunc_dir" ]; then mkdir -p "$zfunc_dir" &>/dev/null; fi
+add_completions() {
+  local file="$zfunc_dir/$1"
+  if [ ! -f "$file" ]; then
+    eval "$2" >"$file"
+  fi
+}
 add_completions _fnm 'fnm completions --shell zsh'
 add_completions _rustup 'rustup completions zsh'
 add_completions _uv 'uv generate-shell-completion zsh'
--- install.sh
+++ install.sh
@@ -20,47 +20,11 @@ echoy() {
     echo -e "\e[33m$1\e[0m"
 }
 
-# Clock animation
-BLA_clock=(🕛 🕐 🕑 🕒 🕓 🕔 🕕 🕖 🕗 🕘 🕙 🕚)
-
 # Use $prefix for commands that might need sudo
 prefix="$([[ $(whoami) == "root" ]] && echo "" || echo "sudo")"
 LAST_OUTPUT=$(mktemp)
 [ -d "$HOME" ] || { echor "$HOME is not a directory. You are cancelled." && exit 1; }
 export PATH="$PATH:/usr/local/go/bin:/usr/local/go/bin:$HOME/go/bin:$HOME/.local/bin"
-
-# Ensure sudo and run command in background with animation, returns 1 if there was an issue
-# $1: command used to check if the 2nd command should be ran ("" is always true)
-# $2: command to run in the background
-# $3: text to append to animation
-# $4: success message
-# $5: failure message (after LAST_OUTPUT)
-# $6: Ignore output if set
-animate() {
-    # Return when check is success
-    [[ -n "$1" ]] && eval "$1" && echob "ℹ️ No action required. $4" && return 0
-
-    # Ensure sudo
-    [[ "$(whoami)" == "root" ]] || sudo -v || exit 1
-
-    # Run with output to tmp
-    eval "$2" &>"$LAST_OUTPUT" &
-
-    # Wait and animate the command
-    local pid=$!
-    while kill -0 $pid 2>/dev/null; do
-        for frame in "${BLA_clock[@]}"; do
-            printf "\r%s %s  " "${frame}" "$3"
-            sleep 0.2
-        done
-    done
-
-    # Clear and output error if needed
-    printf "\033[2K\r"
-    { { wait $pid || [[ -n "$6" ]]; } && echog "✅ $4"; } || { cat "$LAST_OUTPUT" && echor "\n🔴 $5" && return 1; }
-}
-
-echo "Ensuring base tools are installed."
 
 # Install basic tools on debian
 if [ -f /etc/debian_version ]; then
@@ -70,18 +34,17 @@ if [ -f /etc/debian_version ]; then
 
     # Update system
     echop "Detected Debian. Updating and upgrading now!"
-    animate "" \
-        "$installer update && $installer upgrade" \
-        "Updating system" \
-        "System up to date!" \
-        "Failed to update system" || exit 1
+    { tput smcup && echot "ℹ️ Updating system" &&
+        eval "$installer update && $installer upgrade" &&
+        tput rmcup && echog "✅ System up to date"; } ||
+        { echor "🔴 Failed to update system" && exit 1; }
 
     # Install build essential
-    animate "dpkg -s build-essential &>/dev/null" \
-        "$install_cmd build-essential" \
-        "Installing build-essential" \
-        "build-essential installed" \
-        "Failed to install build-essential" || exit 1
+    dpkg -s build-essential &>/dev/null && echot "☑️ build-essential already installed" ||
+        { tput smcup && echot "ℹ️ Installing build-essential" &&
+            eval "$install_cmd build-essential" &&
+            tput rmcup && echog "✅ build-essential installed"; } ||
+        { echor "Failed to install build-essential" && exit 1; }
 
 # Install basic tools for arch
 elif [ -f /etc/arch-release ]; then
@@ -90,167 +53,181 @@ elif [ -f /etc/arch-release ]; then
     install_cmd="$installer -S"
 
     # Update system
-    echot "Arch Detected. You are elite. Updating and upgrading now!"
-    animate "" \
-        "$installer -Syu" \
-        "Updating system" \
-        "System up to date!" \
-        "Failed to update system" || exit 1
+    { tput smcup &&
+        eval "$installer -Syu" &&
+        tput rmcup && echog "✅ System up to date"; } ||
+        { echor "Failed to update" && exit 1; }
 
     # Install base-devel
-    animate "pacman -Q base-devel &>/dev/null" \
-        "$install_cmd base-devel" \
-        "Installing base-devel" \
-        "base-devel installed" \
-        "Failed to install base-devel" || exit 1
+    pacman -Q base-devel &>/dev/null && echob "☑️ base-devel already installed" ||
+        { tput smcup &&
+            eval "$install_cmd base-devel" && echot "ℹ️ Installing base-devel" &&
+            tput rmcup && echog "✅ base-devel installed"; } ||
+        { echor "Failed to install base-devel" && exit 1; }
 
     # Install git
-    animate "command -v git &>/dev/null" \
-        "$install_cmd git" \
-        "Installing git" \
-        "git installed" \
-        "Failed to install git" || exit 1
+    command -v git &>/dev/null && echob "☑️ git already installed" ||
+        {
+            tput smcup && echot "ℹ️ Installing git" &&
+                eval "$install_cmd git" &&
+                tput rmcup && echog "✅ git installed"
+        } ||
+        { echor "🔴 Failed to install git" && exit 1; }
 
     # Install cargo before paru
-    animate "[ -f ~/.cargo/env ]" \
-        "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y" \
-        "Installing cargo" \
-        "cargo installed" \
-        "Failed to install cargo" || exit 1
+    [ -f ~/.cargo/env ] && echob "☑️ cargo already installed" ||
+        { tput smcup && echot "ℹ️ Installing cargo" &&
+            curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &&
+            tput rmcup && echog "✅ cargo installed"; } ||
+        { echor "🔴 Failed to install cargo" && exit 1; }
+    . ~/.cargo/env # Source cargo
 
     # Install paru
     paru_dir="$(mktemp -d)"
-    # command -v paru &>/dev/null ||
-    #     { rm -rf $HOME/paru && git clone https://aur.archlinux.org/paru.git $paru_dir && cd $paru_dir && makepkg -si; } || 
-    #     { exit 1; }
-    animate "command -v paru &>/dev/null" \
-        "rm -rf $HOME/paru && git clone https://aur.archlinux.org/paru.git $paru_dir && cd $paru_dir && makepkg -si --noconfirm" \
-        "Installing paru" \
-        "paru installed" \
-        "Failed to install paru" || exit 1
+    command -v paru &>/dev/null && echob "☑️ paru already installed" ||
+        { tput smcup && echot "ℹ️ Installing paru" &&
+            rm -rf $HOME/paru && git clone https://aur.archlinux.org/paru.git $paru_dir && cd $paru_dir && makepkg -si --noconfirm &&
+            tput rmcup && echog "✅ paru installed"; } ||
+        { echor "🔴 Failed to install paru" && exit 1; }
 fi
 
 # Install zsh
-animate "command -v zsh &>/dev/null" \
-    "$install_cmd zsh" \
-    "Installing zsh" \
-    "zsh installed" \
-    "Failed to install zsh" || exit 1
+command -v zsh &>/dev/null && echob "☑️ zsh already installed" ||
+    { tput smcup && echot "ℹ️ Installing zsh" &&
+        eval "$install_cmd zsh" &&
+        tput rmcup && echog "✅ zsh installed"; } ||
+    { echor "🔴 Failed to install zsh" && exit 1; }
 
 # Install curl (crazy but this happens sometimes)
-animate "command -v curl &>/dev/null" \
-    "$install_cmd curl" \
-    "Installing curl" \
-    "curl installed" \
-    "Failed to install curl" || exit 1
+command -v curl &>/dev/null && echob "☑️ curl already installed" ||
+    { tput smcup && echot "ℹ️ Installing curl" &&
+        eval "$install_cmd curl" &&
+        tput rmcup && echog "✅ curl installed"; } ||
+    { echor "🔴 Failed to install curl" && exit 1; }
 
 # Install unzip
-animate "command -v unzip &>/dev/null" \
-    "$install_cmd unzip" \
-    "Installing unzip" \
-    "unzip installed" \
-    "Failed to install unzip" || exit 1
+command -v unzip &>/dev/null && echob "☑️ unzip already installed" ||
+    { tput smcup && echot "ℹ️ Installing unzip" &&
+        eval "$install_cmd unzip" &&
+        tput rmcup && echog "✅ unzip installed"; } ||
+    { echor "🔴 Failed to install unzip" && exit 1; }
 
 # Install git
-animate "command -v git &>/dev/null" \
-    "$install_cmd git" \
-    "Installing git" \
-    "git installed" \
-    "Failed to install git" || exit 1
+command -v git &>/dev/null && echob "☑️ git already installed" ||
+    { tput smcup && echot "ℹ️ Installing git" &&
+        eval "$install_cmd git" &&
+        tput rmcup && echog "✅ git installed"; } ||
+    { echor "🔴 Failed to install git" && exit 1; }
 
 # Install cargo
-animate "[ -f ~/.cargo/env ]" \
-    "curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y" \
-    "Installing cargo" \
-    "cargo installed" \
-    "Failed to install cargo" || exit 1
-. ~/.cargo/env
+[ -f ~/.cargo/env ] &>/dev/null && echob "☑️ cargo already installed" ||
+    { tput smcup && echot "ℹ️ Installing cargo" &&
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y &&
+        tput rmcup && echog "✅ cargo installed"; } ||
+    { echor "🔴 Failed to install cargo" && exit 1; }
+. ~/.cargo/env # Source cargo
 
 # Install lsd with cargo
-animate "command -v lsd &>/dev/null" \
-    "cargo install lsd" \
-    "Installing lsd" \
-    "lsd installed" \
-    "Failed to install cargo" || exit 1
+command -v lsd &>/dev/null && echob "☑️ lsd already installed" ||
+    { tput smcup && echot "ℹ️ Installing lsd" &&
+        cargo install lsd &&
+        tput rmcup && echog "✅ lsd installed"; } ||
+    { echor "🔴 Failed to install lsd" && exit 1; }
 
 # Install golang
+current_go_version="$(go version 2>/dev/null | awk '{print $3}')"
 latest_go_version="$(curl -fsSL "https://go.dev/VERSION?m=text" | head -1)"
-filename="$latest_go_version.$([[ "$(uname -m)" == "Linux" ]] && { printf "linux"; } || printf "darwin").$(uname -m).tar.gz"
-animate "[[ \"$(go version 2>/dev/null | awk '{print $3}')\" == \"$latest_go_version\" ]]" \
-    "curl -fsSL https://go.dev/dl/go1.26.1.linux-amd64.tar.gz | $prefix tar xvzf - -C /usr/local" \
-    "Installing go" \
-    "go installed" \
-    "Failed to install go" || exit 1
+case "$(uname -s)$(uname -m)" in
+"Linuxx86_64") go_arch="linux-amd64" ;;
+"Linuxaarch64") go_arch="linux-arm64" ;;
+"Darwinx86_64") go_arch="darwin-amd64" ;;
+"Darwinaarch64") go_arch="darwin-arm64" ;;
+*) echor "unknown architecture, don't care" && exit 1 ;;
+esac
+filename="$latest_go_version.$go_arch.tar.gz"
+[[ "$current_go_version" == "$latest_go_version" ]] &>/dev/null && echob "☑️ go ($latest_go_version) already installed" ||
+    { tput smcup && echot "ℹ️ Installing go ($latest_go_version)" &&
+        eval "$prefix rm -rf /usr/local/go && curl -fsSL https://go.dev/dl/$filename | $prefix tar xvzf - -C /usr/local" &&
+        tput rmcup && echog "✅ go installed"; } ||
+    { echor "🔴 Failed to install go" && exit 1; }
 
 # Install fzf (written in go)
-animate "command -v fzf &>/dev/null" \
-    "git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install" \
-    "Installing fzf" \
-    "fzf installed" \
-    "Failed to install fzf"
+[ -d ~/.fzf ] &>/dev/null && echob "☑️ fzf already installed" ||
+    { tput smcup && echot "ℹ️ Installing fzf" &&
+        git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all &&
+        tput rmcup && echog "✅ fzf installed"; } ||
+    { echor "🔴 Failed to install fzf" && exit 1; }
 
 # Install lazygit with go
-animate "command -v lazygit &>/dev/null" \
-    "go install github.com/jesseduffield/lazygit@latest" \
-    "Installing lazygit" \
-    "lazygit installed" \
-    "Failed to install lazygit" || exit 1
+command -v lazygit &>/dev/null && echob "☑️ lazygit already installed" ||
+    { tput smcup && echot "ℹ️ Installing lazygit" &&
+        go install github.com/jesseduffield/lazygit@latest &&
+        tput rmcup && echog "✅ lazygit installed"; } ||
+    { echor "🔴 Failed to install lazygit" && exit 1; }
 
-# Install yazi
-filename="yazi-$(uname -m)-$(
-    { [[ "$(uname -s)" == "Linux" ]] && printf "unknown-linux-%s" "$(ldd --version 2>&1 | grep -qi musl && echo musl || echo gnu)"; } || printf "apple-darwin"
-).zip"
-animate "[ -d /usr/local/yazi ]" \
-    "rm -rf /tmp/yazi* && $prefix rm -rf /usr/local/yazi &&
-        curl -fsSL https://github.com/sxyazi/yazi/releases/download/v26.1.22/yazi-x86_64-unknown-linux-gnu.zip -o /tmp/yazi.zip &&
-        unzip /tmp/yazi.zip -d /tmp && $prefix mv /tmp/yazi-x86_64-unknown-linux-gnu/ /usr/local/yazi/ && $prefix mv /usr/local/yazi/ya* /usr/local/bin" \
-    "Installing yazi" \
-    "yazi installed" \
-    "Failed to install yazi" || exit 1
+# Install yazi if yazi command is found
+# Download yazi zip for identified system build
+# Move file contents into /usr/local/yazi (delete if existing)
+# Move binaries into /usr/local/bin
+case "$(uname -s)$(ldd --version | grep -o GNU)" in
+"LinuxGNU") yazi_arch="unknown-linux-gnu" ;;
+"Linux") yazi_arch="unknown-linux-musl" ;;
+"Darwin") yazi_arch="apple-darwin" ;;
+esac
+yazi_stem="yazi-$(uname -m)-$yazi_arch"
+yazi_link="https://github.com/sxyazi/yazi/releases/download/v26.1.22/$yazi_stem.zip"
+yazi_tmp_dir="$(mktemp -d)"
+yazi_zip_path="$yazi_tmp_dir/yazi.zip"
+command -v yazi &>/dev/null && echob "☑️ yazi already installed" ||
+    { tput smcup && echot "ℹ️ Installing yazi" &&
+        eval "$prefix rm -rf /usr/local/yazi && curl -fsSL $yazi_link -o $yazi_zip_path && 
+            unzip $yazi_zip_path -d $yazi_tmp_dir && $prefix mv $yazi_tmp_dir/$yazi_stem /usr/local/yazi &&
+            $prefix mv -f /usr/local/yazi/* /usr/local/bin" &&
+        tput rmcup && echog "✅ yazi installed"; } ||
+    { echor "🔴 Failed to install yazi" && exit 1; }
 
-# Install fnm
-animate "[ -d ~/.local/share/fnm ]" \
-    "curl -fsSL https://fnm.vercel.app/install | bash" \
-    "Installing fnm" \
-    "fnm installed" \
-    "Failed to install fnm" \
-    "ignore fail"
+# Install fnm if the directory does not already exist at ~/.local/share/fnm
+[ -d ~/.local/share/fnm ] &>/dev/null && echob "☑️ fnm already installed" ||
+    { tput smcup && echot "ℹ️ Installing fnm" &&
+        curl -fsSL https://fnm.vercel.app/install | bash 2>&1 | grep -Pq "Could not infer shell type|open a new terminal" &&
+        tput rmcup && echog "✅ fnm installed"; } ||
+    { echor "🔴 Failed to install fnm" && exit 1; }
 
-# Install uv
-animate "[ -f ~/.local/bin/uv ]" \
-    "curl -LsSf https://astral.sh/uv/install.sh | sh" \
-    "Installing uv" \
-    "uv installed" \
-    "Failed to install uv" || exit 1
+# Install uv if the binary does not exist yet at ~/.local/bin/uv
+[ -f ~/.local/bin/uv ] &>/dev/null && echob "☑️ uv already installed" ||
+    { tput smcup && echot "ℹ️ Installing uv" &&
+        curl -LsSf https://astral.sh/uv/install.sh | sh &&
+        tput rmcup && echog "✅ uv installed"; } ||
+    { echor "🔴 Failed to install uv" && exit 1; }
+
+# Create uv venv in home directory if one does not exist
 [ -f ~/.venv/bin/activate ] || (cd ~ && uv venv &>/dev/null)
 
 # Install zoxide
-animate "[ -f ~/.local/bin/zoxide ]" \
-    "curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh" \
-    "Installing zoxide" \
-    "zoxide installed" \
-    "Failed to install zoxide"
+[ -f ~/.local/bin/zoxide ] && echob "☑️ zoxide already installed" ||
+    { tput smcup && echot "ℹ️ Installing zoxide" &&
+        curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh &&
+        tput rmcup && echog "✅ zoxide installed"; } ||
+    { echor "🔴 Failed to install zoxide" && exit 1; }
 
 # Create backups
 [ -d ~/.config ] || mkdir -p ~/.config
-[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak && echot "Backup ~/.zshrc -> ~/.zshrc.bak"
-[ -f ~/.zshenv ] && mv ~/.zshenv ~/.zshenv.bak && echot "Backup ~/.zshenv -> ~/.zshenv.bak"
-[ -d ~/.config/zsh ] && rm -rf ~/.config/zsh.bak && mv ~/.config/zsh ~/.config/zsh.bak
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak && echot "ℹ️ Backup ~/.zshrc -> ~/.zshrc.bak"
+[ -f ~/.zshenv ] && mv ~/.zshenv ~/.zshenv.bak && echot "ℹ️ Backup ~/.zshenv -> ~/.zshenv.bak"
+[ -d ~/.config/zsh ] && rm -rf ~/.config/zsh.bak && mv ~/.config/zsh ~/.config/zsh.bak && echot "ℹ️ Backup ~/.config/zsh -> ~/.config/zsh.bak"
 
 # Clone config repo
-echo 'export ZDOTDIR=$HOME/.config/zsh' >~/.zshenv
+echo 'export ZDOTDIR=$HOME/.config/zsh' >~/.zshenv && echot "ℹ️ Set ZDOTDIR TO ~/.config/zsh"
 export ZDOTDIR=$HOME/.config/zsh
-animate "" \
-    "git clone https://github.com/huncholane/dotzsh ~/.config/zsh" \
-    "Cloning dotzsh" \
-    "dotzsh cloned. Just a few more things." \
-    "Failed to clone dotzsh" || exit 1
+{ tput smcup && echot "ℹ️ Cloning dotzsh into ~/.config/zsh" &&
+    git clone --depth=1 https://github.com/huncholane/dotzsh ~/.config/zsh &&
+    tput rmcup && echog "✅ dotzsh cloned to ~/.config/zsh"; } ||
+    { echor "🔴 Failed to clone dotzsh into ~/.config/zsh" && exit 1; }
 
 # Install antidote
-animate "[ -d ~/.config/zsh/.antidote ]" \
-    "git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote" \
-    "Installing antidote" \
-    "antidote installed" \
-    "Failed to install antidote" || exit 1
+{ tput smcup && echot "ℹ️ Cloning antidote into ~/.config/zsh/.antidote" &&
+    git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-~}/.antidote &&
+    tput rmcup && echog "✅ antidote installed"; }
 
-echot "\nRun 'exec zsh' to see the magic"
+# echot "\n🚀 Run 'exec zsh' to see the magic"
+exec zsh
